78558d69f3f6e818d37e2ed80fe0a1d80ebcb9c4
Joe Portaro <joe.portaro@pymetrics.com>
2022-03-15 13:09:21 -0400

    Skeleton project to make a react clone

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Create heading node
+const heading = document.createElement('h1')
+heading.textContent = 'Make you a react!'
+
+// Append SVG and heading nodes to the DOM
+const app = document.querySelector('#root')
+app.append(heading)
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+
+    mode: 'development',
+    devServer: {
+        historyApiFallback: true,
+        static: path.resolve(__dirname, './dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+
+    module: {
+        rules: [
+          // JavaScript
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ['babel-loader'],
+          },
+        ],
+      },
+}
